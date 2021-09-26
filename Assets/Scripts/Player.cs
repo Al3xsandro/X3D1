b9485aa867acfdf7436eb13cc288be21723d2230
Alex <alexs123655@gmail.com>
2021-09-26 09:58:07 -0300

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float laneSpeed;
+    private Rigidbody rb;
+    private int currentLane = 1;
+    private Vector3 verticalTargetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ChangeLane(-1);
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ChangeLane(1);
+        }
+
+        Vector3 targetPosition = new Vector3(verticalTargetPosition.x, verticalTargetPosition.y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, laneSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector3.forward * speed;
+    }
+
+    void ChangeLane(int direction)
+    {
+        int targetLane = currentLane + direction;
+        if(targetLane < 0 || targetLane > 2)
+            return;
+        currentLane = targetLane;
+        verticalTargetPosition = new Vector3((currentLane - 1), 0, 0);
+    }
+};
